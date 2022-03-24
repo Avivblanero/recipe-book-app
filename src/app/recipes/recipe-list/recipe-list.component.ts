6201626021939e13d3787bf9bb4f37f9b3166398
Aviv Blanero <avivblanero@gmail.com>
2022-03-24 11:23:00 +0200

    shoping list + ingredients

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('a test recipe', 'this simple a test','https://www.simplyrecipes.com/thmb/mbN8mXZ0srgAT1YrDU61183t0uM=/648x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1b-ea13798d224048b3a28afb0936c9b645.jpg' ),
+    new Recipe('a test recipe', 'this simple a test','https://www.simplyrecipes.com/thmb/mbN8mXZ0srgAT1YrDU61183t0uM=/648x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1b-ea13798d224048b3a28afb0936c9b645.jpg' )
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
